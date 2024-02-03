41b7b958d78fba4c021b4500c952e4f09b25c3ce
Gunk0 <adammacneil_1@outlook.com>
2024-02-03 11:50:15 -0500

    tags updates and fixes to pages

--- scripts/foldersflix.py
+++ scripts/foldersflix.py
@@ -1,46 +1,49 @@
 import os
 import string
 
-folder = '/Volumes/Seagate Backup Plus Drive/WATCH MOVIE/Romance'
-parent = '/Volumes/Seagate Backup Plus Drive/Flix/content/mov'
+folder = '/Volumes/External/Mov/To Add'
 
 def remove(string):
     return string.replace(" ", "-")
 
-
+count = 0 
 dirlist = [item for item in os.listdir(folder) if os.path.isdir(os.path.join(folder,item))]
 for item in dirlist:
     folder=item.split(",")
     folder=item.lstrip("']")
     mov,dire,year=item.split("(")
     movs=remove(mov)
-    path1= os.path.join(parent,movs[:-1])
-    if not os.path.exists(path1):
-        os.mkdir(path1)
-    else:
-        pass
-    path2= os.path.join(path1,"img")
-    if not os.path.exists(path2):
-        os.mkdir(path2)
-    else:
-        pass
-    path3= os.path.join(path1,"sample")
-    if not os.path.exists(path3):
-        os.mkdir(path3)
-    else:
-        pass
+    # count += 1
+    # print(count)
+    if(int(year[:-1]) < 1960):
+        dec = '50s'
+    elif(1960 <= int(year[:-1]) <= 1969):
+        dec = '60s'
+    elif(1970 <= int(year[:-1]) <= 1979):
+        dec = '70s'
+    elif(1980 <= int(year[:-1]) <= 1989):
+        dec = '80s'
+    elif(1990 <= int(year[:-1]) <= 1999):
+        dec = '90s'
+    elif(2000 <= int(year[:-1]) <= 2009):
+        dec = '00s'
+    elif(2010 <= int(year[:-1]) <= 2019):
+        dec = '10s'
+    elif(2020 <= int(year[:-1]) <= 2029):
+        dec = '20s'
 
-    print('<div class="contained">')
+    print('<div class="contained %s">' % dec)
     print('     <div class="container">')
     print('         <img src=content\%s.jpg>'  % movs[:-1])
     print('         <div class="overlay"></div>')
     print('         </div>')
     print('         <a href="https://www.youtube.com/results?search_query=%s">' % movs[:-1])
-    print('             <div class="text">Check out <b>%s</b></div>' % (mov))
+    print('             <div class="text"><b>%s</b></div>' % (mov))
     print('             <p class="text"> Directed by %s (%s)</p>' % (dire[:-2], year[:-1]))
     print('         </a>')
     print('     <div class="bio">bio</div>')
     print('</div>')
     print('')
+
 
 

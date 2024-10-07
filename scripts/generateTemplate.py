a90445e01e4e4c7f81f82871ecca85619d65ad48
gunk0 <152566166+gunk0@users.noreply.github.com>
2024-10-07 13:04:42 -0300

    edits

--- scripts/generateTemplate.py
+++ scripts/generateTemplate.py
@@ -11,27 +11,29 @@ def jpg(string):
 
 # user input
 def startSearch():
-    # Using the Search movie method
-    prompt = input("What movie would you like templated?: ")
-    items = ia.search_movie(prompt)
-    count = 0
-    searched8 = []
-    searchCode = []
-    print("\n")
-    for i in items[0:8]:
-        # print(i.data)
-        count += 1
-        try:
-            searchCode.append(f"{i.movieID}")
-            print(f"{count}: {i} ({i.data['year']})")
-        except:
-            searched8.append(f"{i}")
-            searchCode.append(f"{i.movieID}")
-            print(f"{count}: {i}")
+    x = False
+    while x == False:
+        # Using the Search movie method
+        prompt = input("What movie would you like templated?: ")
+        items = ia.search_movie(prompt)
+        count = 0
+        searched8 = []
+        searchCode = []
+        print("\n")
+        for i in items[0:8]:
+            # print(i.data)
+            count += 1
+            try:
+                searchCode.append(f"{i.movieID}")
+                print(f"{count}: {i} ({i.data['year']})")
+            except:
+                searched8.append(f"{i}")
+                searchCode.append(f"{i.movieID}")
+                print(f"{count}: {i}")
 
-    print("\n")
-    selected = input("which of these movies are you templating? ")
-    startTemplate(searchCode[int(selected)-1])
+        print("\n")
+        selected = input("which of these movies are you templating? ")
+        startTemplate(searchCode[int(selected)-1])
 
 
 
@@ -46,7 +48,8 @@ def startTemplate(movieID):
     title = movie['title']
     year = movie['year']
     director = movie.data['director'][0]['name']
-    director = director[:1]+'.'
+    directorSplit = director.split(' ')[1]
+    director = director[:1]+'. '+directorSplit
     plot = movie.data['plot'][0]
     genres = movie.data['genres']
     youtube = plus(title)
@@ -88,8 +91,8 @@ def startTemplate(movieID):
 
     # print(f"{title} - {youtube} - {year} - {director} - {plot} - {genreTag}")
     print(f"<div id='mov' class='contained Recent {dec} {genre}'> \n    <div class=container> \n        <img src='content\{image}.jpg'> \n        <div class='overlay'></div> \n    </div> \n    <a target='_blank' href='https://www.youtube.com/results?search_query={youtube}'> \n        <div class='text'><b>{title}</b></div>\n <p class='text'> Directed by {director} ({year})</p>\n    </a>\n    <div class='bio'>{plot} \n    <br><br>{genreTag}</div>\n</div>")
-    time.sleep(5)
-    webbrowser.open_new(f"https://duckduckgo.com/?q={youtube}")
+    # time.sleep(5)
+    # webbrowser.open_new(f"https://duckduckgo.com/?q={youtube}")
 
 
 def main():
